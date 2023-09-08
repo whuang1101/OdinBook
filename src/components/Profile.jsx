79a7454e81c57e62c6b817ac81421e62a6d1bf19
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-09-08 15:09:07 -0400

    Bottom section of profile page done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,17 @@
 import Header from "./Header"
 import "../css/profile.css"
 import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import Icon from '@mdi/react';
-import { mdiPencil } from '@mdi/js';
+import { mdiBriefcase, mdiHome, mdiPencil, mdiSchool } from '@mdi/js';
+import { updatePage } from "../redux/pageSlice"
 const Profile = ({setUser}) => {
     const {id} = useParams()
     const host = useSelector(state => state.host);
+    const user = useSelector(state => state.user);
     const [profile, setProfile] = useState(null);
+    const dispatch = useDispatch();
     useEffect(()=> {
         fetch(`${host}/users/${id}`).then(
             response => {
@@ -21,6 +24,7 @@ const Profile = ({setUser}) => {
                 }
             }
         ).then(data => setProfile(data)).catch(err => console.error(err));
+        dispatch(updatePage("profile"))
     },[])
 
     return(
@@ -28,30 +32,81 @@ const Profile = ({setUser}) => {
             <Header setUser={setUser}/>
             {profile && 
             <div className="profile-screen">
-                <div className="profile-container">
-                    <div className="background-image">
-                    </div>
-                    <div className="profile-display-container">
-                        <div className="profile-header">
-                            <div className="image-profile">
-                                <img src={profile.image_url} alt={profile.name} className="medium-size"/>
-                            </div>
-                            <div className="profile-page-name">
-                                <div className="user-name">
-                                    {profile.name}
+                <div className="profile-box-shadow">
+                    <div className="profile-container">
+                        <div className="background-image">
+                        </div>
+                        <div className="profile-display-container">
+                            <div className="profile-header">
+                                <div className="image-profile">
+                                    <img src={profile.image_url} alt={profile.name} className="medium-size"/>
                                 </div>
-                                <div className="num-friends">
-                                    {profile.friends_list.length} friends
+                                <div className="profile-page-name">
+                                    <div className="user-name">
+                                        {profile.name}
+                                    </div>
+                                    <div className="num-friends">
+                                        {profile.friends_list.length} friends
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="profile-edit">
-                                <button className="edit-profile"> <Icon path={mdiPencil} size={1} />
-Edit Profile</button>
+                                {user._id === profile._id &&
+                                    <div className="profile-edit">
+                                        <button className="edit-profile"> 
+                                        <Icon path={mdiPencil} size={1} />
+                                        Edit Profile</button>
+                                    </div>
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>}
+                <div className="info-bottom">
+                    <div className="info-container">
+                        <div className="info-post">
+                            <div className="info-section">
+                                <h3>Info</h3>
+
+                                {profile.studies_at &&
+                                    <div className="university">
+                                        <Icon path={mdiSchool} size={1} color={"rgb(140,147,157)"}/>
+                                        <div className="university-info">
+                                            Studied at {profile.studies_at}
+                                        </div>
+                                    </div>
+                                }
+                                {profile.lives &&
+                                    <div className="lives">
+                                        <Icon path={mdiHome} size={1} color={"rgb(140,147,157)"}/>
+                                        <div className="live-info">
+                                            Lives in {profile.lives}
+                                        </div>
+                                    </div>
+                                }
+                                {profile.job &&
+                                    <div className="lives">
+                                        <Icon path={mdiBriefcase} size={1} color={"rgb(140,147,157)"}/>
+                                        <div className="live-info">
+                                            Works at {profile.job}
+                                        </div>
+                                    </div>
+                                }
+                                {
+                                ((user._id === profile._id) &&(profile.job || profile.lives || profile.studies_at)) &&
+                                <button className="edit-details">
+                                    <div className="edit">
+                                        Edit Details
+                                    </div>
+                                </button>
+                                }
+                            </div>
+                            <div className="post-section">
+   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            }
             
         </>
     )
